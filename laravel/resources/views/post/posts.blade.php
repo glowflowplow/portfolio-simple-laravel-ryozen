32bd08511f781112c1e97e5d2dbdb4c7f5811b17
plow <plowflowglow+github@gmail.com>
2021-01-26 04:07:17 +0000

    apply layout to post list page

--- laravel/resources/views/post/posts.blade.php
+++ laravel/resources/views/post/posts.blade.php
@@ -1,130 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
-    <!-- Scripts -->
-    <script src="{{ mix('js/app.js') }}" defer></script>
-
-    <style>
-        body {
-            font-family: 'Nunito';
-        }
-    </style>
-</head>
-
-<body class="antialiased">
-    <!-- Navigation Bar-->
-    <nav class="bg-gray-800">
-        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div class="relative flex items-center justify-between h-16">
-                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                    <!-- Mobile menu button-->
-
-                    <button
-                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                        aria-expanded="false">
-                        <span class="sr-only">Open main menu</span>
-                        <!-- Icon when menu is closed. -->
-                        <!--
-                            Heroicon name: menu
-                            Menu open: "hidden", Menu closed: "block"
-                        -->
-                        <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                        <!-- Icon when menu is open. -->
-                        <!--
-                            Heroicon name: x
-                            Menu open: "block", Menu closed: "hidden"
-                        -->
-                        <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
-                </div>
-                <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                    <!-- PC Browser -->
-
-                    <div class="hidden sm:block sm:ml-6">
-                        <div class="flex space-x-4">
-                            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <a href="{{ route('home') }}"
-                                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">HOME</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <!-- Profile dropdown -->
-                    <div class="ml-3 relative" x-data="{show: false}">
-                        <div>
-                            <button x-on:click="show=true"
-                                class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                                id="user-menu" aria-haspopup="true">
-                                <span class="sr-only">Open user menu</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke="currentColor" class="h-8 w-8 rounded-full text-gray-700 bg-gray-300">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                                </svg>
-                            </button>
-                        </div>
-                        <div x-show="show" x-on:click.away="show=false"
-                            class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5"
-                            role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                            <a href="{{ url('/dashboard') }}"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                role="menuitem">Dashboard</a>
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-                                <a href="{{ route('logout') }}" onclick="
-                                        event.preventDefault();
-                                        this.closest('form').submit();"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                                    {{ __('Sign out') }}
-                                </a>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!--
-            Mobile menu, toggle classes based on menu state.
-            Menu open: "block", Menu closed: "hidden"
-        -->
-        <div class="hidden sm:hidden">
-            <div class="px-2 pt-2 pb-3 space-y-1">
-                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-                    HOME</a>
-                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2
-                    rounded-md text-base font-medium">Team</a>
-                <a href="#"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-                    Projects</a>
-                <a href="#"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
-            </div>
-        </div>
-    </nav>
-
-    <!-- Content -->
+<x-app-layout>
+    <x-slot name="header">
+        Post
+    </x-slot>
     <div class="flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
         <div class="w-3/4 mt-12">
             <div class="justify-start">
@@ -167,7 +44,8 @@
                                         <td class="py-4 whitespace-nowrap text-center text-sm font-medium">
                                             <form action="/posts/{{ $post->id }}/delete" method="post">
                                                 @csrf
-                                                <a href="{{ url('/posts') }}" class="text-indigo-600 hover:text-indigo-900" onclick="
+                                                <a href="{{ url('/posts') }}"
+                                                    class="text-indigo-600 hover:text-indigo-900" onclick="
                                                     event.preventDefault();
                                                     this.closest('form').submit();">
                                                     Delete
@@ -184,6 +62,4 @@
             </div>
         </div>
     </div>
-</body>
-
-</html>
+</x-app-layout>
